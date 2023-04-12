2d534bda1c63eba2469bf4e71dfa4c3af953489b
DhayuIntan <intaandh@gmail.com>
2023-04-12 21:57:17 +0700

    seeder user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             TransaksiSeeder::class,
             MobilSeeder::class,
+            UserSeeder::class,
         ]);
 
     }
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MobilController;
 use App\Http\Controllers\TransaksiController;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,8 +17,17 @@ use App\Http\Controllers\TransaksiController;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
+//Auth::routes();
+Route::get('/logout', [LoginController::class, 'logout']);
+Route::middleware(['auth'])->group(function(){
+    Route::get('/', function () {
+        return view('index');
+    });
+    Route::resource('/mobil', MobilController::class);
+    Route::resource('/transaksi', TransaksiController::class);
 });
-Route::resource('/mobil', MobilController::class);
-Route::resource('/transaksi', TransaksiController::class);
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
